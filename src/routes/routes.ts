3e679b80ce6b4ca4fe635827ad49ffca81da142c
DESKTOP-1BUG11F\Mini Meachine 15 <zaimiftikhar0786@gmail.com>
2025-06-05 05:20:58 -0700

    Updated routes and added new routes for task

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,11 @@
-import authRoutes from "./auth.route";
-import userRoutes from './user.route';
-import todoRoutes from './todo.route';
+import express from 'express';
+import authRouter from './auth.route';    
+import userRouter from './user.route';    
+import taskRouter from './task.route';    
 
-const routes = [
-    authRoutes,
-    userRoutes,
-    todoRoutes
+const mainRouter = express.Router();
+mainRouter.use('/auth', authRouter);
+mainRouter.use('/users', userRouter);
+mainRouter.use('/tasks/', taskRouter);
 
-]
-
-export default routes;
+export default mainRouter; 
