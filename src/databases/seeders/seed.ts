0c67ca6c364d32af2a72ab33fe45cb8d1c23d68b
DESKTOP-R96JTS2\latitude 5300 <talhaahsanshiekh723@gmail.com>
2025-06-24 01:15:10 +0500

    UPDATE SEEDERS

--- src/databases/seeders/seed.ts
+++ src/databases/seeders/seed.ts
@@ -1,25 +1,37 @@
 import { connectDB, disconnectDB } from '../../config/db.config';
 import { seedUsers } from './user.seeder';
 import { seedTodos } from './todo.seeder';
+import RedisClient from '../../lib/redis_client';
 
 const seed = async () => {
   console.log('🚀 Starting seeding...');
 
   try {
     await connectDB();
+    const redis = RedisClient.getInstance();
 
     console.log('\n👤 Seeding users...');
     const users = await seedUsers();
 
+    console.log('\n📦 Caching user data in Redis...');
+    for (const user of users) {
+      await redis.set(`user:${user._id}:email`, user.email);
+    }
+
     console.log('\n📝 Seeding todos...');
     await seedTodos(users);
 
+    await redis.set('app:totalSeededUsers', users.length.toString());
+
     console.log('\n🎉 Seeding completed!');
-  } catch (err) {
-    console.error('❌ Seeding failed:', err);
-  } finally {
     await disconnectDB();
     console.log('🏁 Finished seeding process.');
+
+    process.exit(0); // ✅ Success
+  } catch (err) {
+    console.error('❌ Seeding failed:', err);
+    await disconnectDB();
+    process.exit(1); // ❌ Failure
   }
 };
 
